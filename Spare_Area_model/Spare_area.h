df4c79edd87a9ac50a18a3a0950e90a46e1d74c1
hyung8789 <44813321+hyung8789@users.noreply.github.com>
2020-11-15 18:59:32 +0900

    final

---
+++ Spare_Area_model/Spare_area.h
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <windows.h>
+
+#define SPARE_AREA_BYTE 16
+#define SPARE_AREA_BIT 128
+
+#define BLOCK_PER_SECTOR 32
+#define SECTOR_PER_BYTE 512 //한 섹터의 데이터가 기록되는 영역의 크기 (byte)
+#define SECTOR_INC_SPARE_BYTE 528 //Spare 영역을 포함한 한 섹터의 크기 (byte)
+
+//8비트 크기의 Spare Area 초기값 지정
+#define SPARE_INIT_VALUE (0xff) //0xff(16) = 255(10) = 11111111(2)
+#define TRUE_BIT (0x1)
+#define FALSE_BIT (0x0)
+
+#define SUCCESS 1
+#define COMPLETE 0
+#define FAIL -1
+
+/***
+* 초기값 모두 0x1로 초기화
+* 블록 단위 Erase 연산 수행 시 모두 0x1로 초기화
+ ---------
+* 각 섹터(페이지)의 Spare Area의 전체 16byte에 대해 첫 1byte를 블록 및 섹터(페이지)의 상태 정보로 사용
+* BLOCK_TYPE(Normal or Spare, 1bit) || IS_VALID (BLOCK, 1bit) || IS_EMPTY (BLOCK, 1bit) || IS_VALID (SECTOR, 1bit) || IS_EMPTY(SECTOR, 1bit) || DUMMY (3bit)
+* 기타, Block Address(Logical) Area, ECC Area 등 추가적으로 사용 시, META_DATA 구조체, SPARE_read, SPARE_write 수정
+***/
+
+enum class BLOCK_STATE : const unsigned //블록 상태 정보
+{
+	/***
+	* 각 블록의 첫 번째 섹터(페이지)의 Spare 영역에 해당 블록 정보를 관리
+	* 일반 데이터 블록 혹은 직접적 데이터 기록이 불가능한 Spare 블록에 대하여,
+	* EMPTY : 해당 블록은 블록 내의 모든 오프셋에 대해 비어있고, 유효하다. (초기 상태)
+	* VALID : 해당 블록은 유효하고, 일부 유효한 데이터가 기록되어 있다.
+	* INVALID : 해당 블록 내의 모든 오프셋에 대해 무효하거나, 일부 유효 데이터를 포함하고 있지만 더 이상 사용 불가능. 재사용 위해서 블록 단위 Erase 수행하여야 함
+	***/
+
+	/***
+	* BLOCK_TYPE || IS_VALID || IS_EMPTY
+	* 블록에 대한 6가지 상태, 2^3 = 8, 3비트 필요
+	***/
+
+	NORMAL_BLOCK_EMPTY = (0x7), //초기 상태, 0x7(16) = 7(10) = 111(2)
+	NORMAL_BLOCK_VALID = (0x6), //0x6(16) = 6(10) = 110(2)
+	NORMAL_BLOCK_INVALID = (0x4), //0x4(16) = 4(10) = 100(2)
+	SPARE_BLOCK_EMPTY = (0x3), //0x3(16) = 3(10) = 011(2)
+	SPARE_BLOCK_VALID = (0x2), //0x2(16) = 2(10) = 010(2)
+	SPARE_BLOCK_INVALID = (0x0) //0x0(16) = 0(10) = 000(2)
+};
+
+enum class SECTOR_STATE : const unsigned //섹터(페이지) 상태 정보
+{
+	/***
+	* 모든 섹터(페이지)에서 관리
+	* EMPTY : 해당 섹터(페이지)는 비어있고, 유효하다. (초기 상태)
+	* VALID : 해당 섹터(페이지)는 유효하고, 유효한 데이터가 기록되어 있다.
+	* INVALID : 해당 섹터(페이지)는 무효하고, 무효한 데이터가 기록되어 있다. 재사용 위해서 블록 단위 Erase 수행하여야 함
+	***/
+
+	/***
+	* IS_VALID || IS_EMPTY
+	* 섹터에 대한 3가지 상태, 2^2 = 4, 2비트 필요
+	***/
+
+	EMPTY = (0x3), //초기 상태, 0x3(16) = 3(10) = 11(2)
+	VALID = (0x2), //0x2(16) = 2(10) = 10(2)
+	INVALID = (0x0) //0x0(16) = 0(10) = 00(2)
+};
+
+struct META_DATA
+{
+	BLOCK_STATE block_state; //블록 상태
+	SECTOR_STATE sector_state; //섹터 상태
+};
+
+int SPARE_read(unsigned char* spare_area_pos, META_DATA*& dst_meta_buffer);
+int SPARE_write(unsigned char* spare_area_pos, META_DATA*& src_meta_buffer);
+void print_meta_info(META_DATA*& src_data);
+
+void bitdisp(int c, int start_digits, int end_digits);
---
+++ Spare_Area_model/main.cpp
@@ -0,0 +1,352 @@
+﻿#include "Spare_area.h"
+
+unsigned char sector[SECTOR_INC_SPARE_BYTE] = { NULL, };
+
+/***
+	AND : 둘 다 1인 경우메만 1
+	OR : 둘 다 0인 경우만 0
+	XOR : 둘 다 같은 경우만 0
+	~ : 반전
+***/
+
+void bitdisp(int c, int start_digits, int end_digits) {
+	printf("bits : ");
+	for (int i = start_digits; i >= end_digits; i--)
+		printf("%1d", (c) & (0x1 << (i)) ? 1 : 0);
+	printf("\n");
+}
+
+int SPARE_read(unsigned char* spare_area_pos, META_DATA*& dst_meta_buffer)
+{
+	unsigned char bits_8_buffer; //1byte == 8bit크기의 블록 및 섹터(페이지) 정보에 관하여 Spare Area를 읽어들인 버퍼 
+	unsigned block_state_buffer = SPARE_INIT_VALUE; //블록 상태 버퍼, 0x7(16) = 7(10) = 111(2)
+	unsigned sector_state_buffer = SPARE_INIT_VALUE; //섹터 상태 버퍼, 0x3(16) = 3(10) = 11(2)
+	//__int8 state_buffer_bit_digits; //블록 상태 버퍼, 섹터 상태 버퍼의 비트 자리 위치 카운터
+
+	//현재 Spare area의 1바이트만 사용하므로 해당 부분만 읽는다
+	bits_8_buffer = spare_area_pos[SECTOR_PER_BYTE]; //read Spare area pos
+
+	if (dst_meta_buffer == NULL)
+		dst_meta_buffer = new META_DATA;
+	else
+		goto MEM_LEAK_ERR;
+
+
+	/*** 읽어들인 8비트(2^7 ~2^0)에 대해서 블록 상태(2^7 ~ 2^5) 판별 ***/
+	block_state_buffer = (((bits_8_buffer) >> (5)) & (0x7)); //추출 끝나는 2^5 자리가 LSB에 오도록 오른쪽으로 5번 쉬프트하여, 00000111(2)와 AND 수행
+	/*
+	state_buffer_bit_digits = 2; //블록 상태 버퍼의 2^2 자리 위치부터
+	for (__int8 bit_digits = 7; bit_digits >= 5; bit_digits--) //블록 상태 정보에 대하여
+	{
+		switch ((bits_8_buffer) & (TRUE_BIT << (bit_digits)))
+		{
+		case TRUE_BIT: //이미 0x1이므로 자릿수만 감소
+			state_buffer_bit_digits--;
+			break;
+
+		case FALSE_BIT: //해당 위치에 ^(exclusive not) 연산
+			block_state_buffer ^= 0x1 << state_buffer_bit_digits;
+			state_buffer_bit_digits--;
+			break;
+
+		default:
+			printf("판별실패\n");
+			bitdisp(block_state_buffer, 3,0);
+			system("pause);
+			break;
+		}
+	}
+	*/
+	switch (block_state_buffer) //블록 상태 설정
+	{
+	case (const unsigned)BLOCK_STATE::NORMAL_BLOCK_EMPTY:
+		dst_meta_buffer->block_state = BLOCK_STATE::NORMAL_BLOCK_EMPTY;
+		break;
+
+	case (const unsigned)BLOCK_STATE::NORMAL_BLOCK_VALID:
+		dst_meta_buffer->block_state = BLOCK_STATE::NORMAL_BLOCK_VALID;
+		break;
+
+	case (const unsigned)BLOCK_STATE::NORMAL_BLOCK_INVALID:
+		dst_meta_buffer->block_state = BLOCK_STATE::NORMAL_BLOCK_INVALID;
+		break;
+
+	case (const unsigned)BLOCK_STATE::SPARE_BLOCK_EMPTY:
+		dst_meta_buffer->block_state = BLOCK_STATE::SPARE_BLOCK_EMPTY;
+		break;
+
+	case (const unsigned)BLOCK_STATE::SPARE_BLOCK_VALID:
+		dst_meta_buffer->block_state = BLOCK_STATE::SPARE_BLOCK_VALID;
+		break;
+
+	case (const unsigned)BLOCK_STATE::SPARE_BLOCK_INVALID:
+		dst_meta_buffer->block_state = BLOCK_STATE::SPARE_BLOCK_INVALID;
+		break;
+
+	default:
+		printf("Block State ERR\n");
+		goto WRONG_META_ERR;
+	}
+		
+	/*** 읽어들인 8비트(2^7 ~2^0)에 대해서 섹터 상태(2^4 ~ 2^3) 판별 ***/
+	sector_state_buffer = (((bits_8_buffer) >> (3)) & (0x3)); //추출 끝나는 2^3 자리가 LSB에 오도록 오른쪽으로 3번 쉬프트하여, 00000011(2)와 AND 수행
+	/*
+	state_buffer_bit_digits = 1; //섹터 상태 버퍼의 2^1 자리 위치부터
+	for (__int8 bit_digits = 4; bit_digits >= 3; bit_digits--) //섹터(페이지) 상태 정보에 대하여
+	{
+		switch ((bits_8_buffer) & (TRUE_BIT << (bit_digits)))
+		{
+		case TRUE_BIT: //이미 0x1이므로 자릿수만 감소
+			state_buffer_bit_digits--;
+			break;
+
+		case FALSE_BIT: //해당 위치에 ^(exclusive not) 연산
+			sector_state_buffer ^= 0x1 << state_buffer_bit_digits;
+			state_buffer_bit_digits--;
+			break;		
+		
+		default:
+			printf("판별실패\n");
+			bitdisp(sector_state_buffer, 1,0);
+			system("pause);
+		}
+	}
+	*/
+	switch (sector_state_buffer) //섹터(페이지) 상태 설정
+	{
+	case (const unsigned)SECTOR_STATE::EMPTY:
+		dst_meta_buffer->sector_state = SECTOR_STATE::EMPTY;
+		break;
+
+	case (const unsigned)SECTOR_STATE::VALID:
+		dst_meta_buffer->sector_state = SECTOR_STATE::VALID;
+		break;
+
+	case (const unsigned)SECTOR_STATE::INVALID:
+		dst_meta_buffer->sector_state = SECTOR_STATE::INVALID;
+		break;
+
+	default:
+		printf("Sector State ERR\n");
+		bitdisp(sector_state_buffer, 1,0);
+		goto WRONG_META_ERR;
+	}
+
+	// 기타 Meta 정보 추가 시 읽어서 처리 할 코드 추가
+
+	return SUCCESS;
+
+WRONG_META_ERR: //잘못된 meta정보 오류
+	fprintf(stderr, "오류 : 잘못된 meta 정보 (SPARE_read)\n");
+	system("pause");
+	exit(1);
+
+MEM_LEAK_ERR:
+	fprintf(stderr, "오류 : meta 정보에 대한 메모리 누수 발생 (SPARE_read)\n");
+	system("pause");
+	exit(1);
+
+}
+
+int SPARE_write(unsigned char* spare_area_pos, META_DATA*& src_meta_buffer)
+{
+	if (src_meta_buffer == NULL)
+		return FAIL;
+
+	// Spare Area의 전체 16byte에 대해 첫 1byte를 블록 및 섹터(페이지)의 상태 정보에 대한 처리
+	// BLOCK_TYPE(Normal or Spare, 1bit) || IS_VALID (BLOCK, 1bit) || IS_EMPTY (BLOCK, 1bit) || IS_VALID (SECTOR, 1bit) || IS_EMPTY(SECTOR, 1bit) || DUMMY (3bit)
+	unsigned bits_8_buffer = ~(SPARE_INIT_VALUE); //1byte == 8bit크기의 블록 및 섹터(페이지) 정보에 관하여 Spare Area에 기록 할 버퍼, 00000000(2)
+
+	switch (src_meta_buffer->block_state) //1바이트 크기의 bits_8_buffer에 대하여
+	{
+	case BLOCK_STATE::NORMAL_BLOCK_EMPTY: //2^7, 2^6, 2^5 비트를 0x1으로 설정
+		bits_8_buffer |= (0x7 << 5); //111(2)를 5번 왼쪽 쉬프트하여 11100000(2)를 OR 수행
+		break;
+
+	case BLOCK_STATE::NORMAL_BLOCK_VALID: //2^7, 2^6 비트를 0x1로 설정
+		bits_8_buffer |= (0x6 << 5); //110(2)를 5번 왼쪽 쉬프트하여 11000000(2)를 OR 수행
+		break;
+
+	case BLOCK_STATE::NORMAL_BLOCK_INVALID: //2^7 비트를 0x1로 설정
+		bits_8_buffer |= (TRUE_BIT << 7); //10000000(2)를 OR 수행
+		break;
+
+	case BLOCK_STATE::SPARE_BLOCK_EMPTY: //2^6, 2^5 비트를 0x1로 설정
+		bits_8_buffer |= (0x6 << 4); //110(2)를 4번 왼쪽 쉬프트하여 01100000(2)를 OR 수행
+		break;
+
+	case BLOCK_STATE::SPARE_BLOCK_VALID: //2^6 비트를 0x1로 설정
+		bits_8_buffer |= (TRUE_BIT << 6); //01000000(2)를 OR 수행
+		break;
+
+	case BLOCK_STATE::SPARE_BLOCK_INVALID: //do nothing
+		break;
+
+	default:
+		goto WRONG_META_ERR;
+	}
+
+	switch (src_meta_buffer->sector_state) //1바이트 크기의 bits_8_buffer에 대하여
+	{
+	case SECTOR_STATE::EMPTY: //2^4, 2^3 비트를 0x1로 설정
+		bits_8_buffer |= (0x3 << 4); //11(2)를 3번 왼쪽 쉬프트하여 00011000(2)를 OR 수행
+		break;
+
+	case SECTOR_STATE::VALID: //2^4비트를 0x1로 설정
+		bits_8_buffer |= (TRUE_BIT << 4); //00010000(2)를 OR 수행
+		break;
+
+	case SECTOR_STATE::INVALID: //do nothing
+		break;
+
+	default:
+		goto WRONG_META_ERR;
+	}
+	
+	// DUMMY 3비트 처리 (2^2 ~ 2^0)
+	bits_8_buffer |= (0x7); //00000111(2)를 OR 수행
+
+	// 기타 Meta 정보 추가 시 기록 할 코드 추가
+
+	spare_area_pos[SECTOR_PER_BYTE] = bits_8_buffer;
+
+	return SUCCESS;
+
+WRONG_META_ERR: //잘못된 meta정보 오류
+	fprintf(stderr, "오류 : 잘못된 meta 정보 (SPARE_write)\n");
+	system("pause");
+	exit(1);
+
+}
+
+void print_meta_info(META_DATA*& src_data)
+{
+	printf("Block State : ");
+
+	switch (src_data->block_state)
+	{
+	case BLOCK_STATE::NORMAL_BLOCK_EMPTY:
+		printf("NORMAL_BLOCK_EMPTY\n");
+		break;
+
+	case BLOCK_STATE::NORMAL_BLOCK_VALID:
+		printf("NORMAL_BLOCK_VALID\n");
+		break;
+
+	case BLOCK_STATE::NORMAL_BLOCK_INVALID:
+		printf("NORMAL_BLOCK_INVALID\n");
+		break;
+
+	case BLOCK_STATE::SPARE_BLOCK_EMPTY:
+		printf("SPARE_BLOCK_EMPTY\n");
+		break;
+
+	case BLOCK_STATE::SPARE_BLOCK_VALID:
+		printf("SPARE_BLOCK_VALID\n");
+		break;
+
+	case BLOCK_STATE::SPARE_BLOCK_INVALID:
+		printf("SPARE_BLOCK_INVALID\n");
+		break;
+
+	}
+
+	printf("Sector State : ");
+	switch (src_data->sector_state)
+	{
+	case SECTOR_STATE::EMPTY:
+		printf("EMPTY\n");
+		break;
+
+	case SECTOR_STATE::VALID:
+		printf("VALID\n");
+		break;
+
+	case SECTOR_STATE::INVALID:
+		printf("INVALID\n");
+		break;
+	}
+	
+}
+
+void main()
+{
+	META_DATA* meta_data = NULL;
+	
+	system("pause"); //snapshot pos
+	//init
+	for (int byte_unit = SECTOR_PER_BYTE; byte_unit < SECTOR_INC_SPARE_BYTE; byte_unit++) //섹터 내(0~527)의 512 ~ 527 까지 Spare Area에 대해 할당
+	{
+		sector[byte_unit] = SPARE_INIT_VALUE; //0xff(16) = 11111111(2) = 255(10) 로 초기화
+	}
+
+	/***
+		AND : 둘 다 1인 경우메만 1
+		OR : 둘 다 0인 경우만 0
+		XOR : 둘 다 같은 경우만 0
+		~ : 반전
+	***/
+
+	bitdisp(sector[512], 7, 0);
+
+	//읽기 테스트
+	printf("읽기 테스트\n");
+	SPARE_read(sector, meta_data);
+	print_meta_info(meta_data);
+	delete meta_data;
+	meta_data = NULL;
+
+	sector[512] &= ((0xff) << (8)); //전체 비트 반전
+	bitdisp(sector[512], 7,0);
+
+	SPARE_read(sector, meta_data);
+	print_meta_info(meta_data);
+	delete meta_data;
+	meta_data = NULL;
+
+	sector[512] = SPARE_INIT_VALUE; //초기화
+	bitdisp(sector[512], 7,0);
+
+	//쓰기 테스트
+	printf("\n쓰기 테스트\n");
+	SPARE_read(sector, meta_data);
+
+	printf("--change state 1--\n");
+	meta_data->sector_state = SECTOR_STATE::VALID;
+	meta_data->block_state = BLOCK_STATE::SPARE_BLOCK_INVALID;
+	print_meta_info(meta_data);
+
+	printf("\nSpare_write 수행\n");
+	SPARE_write(sector, meta_data);
+	
+	delete meta_data;
+	meta_data = NULL;
+	
+	SPARE_read(sector, meta_data);
+	print_meta_info(meta_data);
+
+	printf("--change state 2--\n");
+	meta_data->sector_state = SECTOR_STATE::INVALID;
+	meta_data->block_state = BLOCK_STATE::NORMAL_BLOCK_INVALID;
+	print_meta_info(meta_data);
+
+	printf("\nSpare_write 수행\n");
+	SPARE_write(sector, meta_data);
+
+	delete meta_data;
+	meta_data = NULL;
+
+	SPARE_read(sector, meta_data);
+	print_meta_info(meta_data);
+
+	for (int i = 0; i <= 100000; i++) //memleak test
+	{
+		delete meta_data;
+		meta_data = NULL;
+		SPARE_read(sector, meta_data);
+	}
+	delete meta_data;
+	meta_data = NULL;
+
+	system("pause");
+}
